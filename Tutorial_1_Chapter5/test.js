e53eebcb3fe9b640f3a2a5c45074763663b37a10
Nyakallo1996 <Lekgoabamahlaks@gmail.com>
2024-04-19 11:46:45 +0200

    node

--- Tutorial_1_Chapter5/test.js
+++ Tutorial_1_Chapter5/test.js
@@ -10,3 +10,22 @@ BlogPost.create({
     console.log(error, blogpost)
 })
 
+//To select all documents in BlogPosts collection: pass an empty document as query filter parameter to the first argument of the find method
+BlogPost.find({}, (error, blogpost) => {
+    console.log(error, blogpost)
+})
+
+//to find all documents in BlogPosts collection with a particular title for e.g.
+BlogPost.find({
+    title: "The Mythbuster's Guide to Saving Money on Energy Bills"
+}, (error, blogpost) => {
+    console.log(error, blogpost)
+})
+
+//Or
+
+/*BlogPost.find({
+    tilte:/The/}, (error, blogpost) => {
+        console.log(error, blogpost)
+    }
+})*/
