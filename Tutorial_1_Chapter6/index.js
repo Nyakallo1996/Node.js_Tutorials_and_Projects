b3380f122a46558ec2c51719852e625d69f017aa
Nyakallo1996 <Lekgoabamahlaks@gmail.com>
2024-04-22 11:34:07 +0200

    node

--- Tutorial_1_Chapter6/index.js
+++ Tutorial_1_Chapter6/index.js
@@ -3,15 +3,27 @@ const path = require('path');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 const app = new express();
+const bodyParser = require("body-parser");
+const BlogPost = require("./models/BlogPost.js")
+
 app.use(express.static('public'));
 // With app.set('view engine','ejs'), we tell Express to use EJS as our templating engine, 
 // that any file ending in .ejs should be rendered with the EJS package.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}))
 app.set('view engine', 'ejs');
 mongoose.connect('mongodb://127.0.0.1/my_database', {useNewUrlParser: true});
 
-app.get('/', (req, res) => {
-    res.render('index');
-});
+//app.get('/', (req, res) => {
+//    res.render('index');
+//});
+
+app.get("/", async (req, res) => {
+    const blogposts = await BlogPost.find({})
+    res.render("index", {
+        blogposts
+    });
+})
 
 app.get('/about', (req, res) => {
     res.render('about');
@@ -28,6 +40,18 @@ app.get('/post', (req, res) => {
 app.get("/posts/new", (req, res) => {
     res.render("create");
 });
+
+app.post("/posts/store", (req, res) => {
+    console.log(req.body);
+    res.redirect("/");
+});
+
+app.post("/posts/store", async (req, res) => {
+    await BlogPost.create(req.body)
+    res.redirect("/")
+})
+
+
 
 app.listen(3000, () => {
     console.log("App listening on port 3000");
