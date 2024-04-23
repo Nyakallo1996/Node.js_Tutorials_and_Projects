163c618e46fd76c18ba40fbfe622bc9531130f0e
Nyakallo1996 <Lekgoabamahlaks@gmail.com>
2024-04-23 12:03:21 +0200

    node

--- Tutorial_1_Chapter6/index.js
+++ Tutorial_1_Chapter6/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+const BlogPost = require('./models/BlogPost.js') 
 const app = new express();
 
 app.use(express.static('public'));
@@ -12,9 +13,16 @@ app.use(express.urlencoded());
 app.set('view engine', 'ejs');
 mongoose.connect('mongodb://127.0.0.1/my_database', {useNewUrlParser: true});
 
-app.get('/', (req, res) => {
-    res.render('index');
-});
+//app.get('/', (req, res) => {
+//    res.render('index');
+//});
+
+app.get('/',async (req,res)=>{     
+    const blogposts = await BlogPost.find({})      
+    res.render('index',{ 
+    blogposts: blogposts 
+    }); 
+    }) 
 
 app.get('/about', (req, res) => {
     res.render('about');
@@ -32,10 +40,15 @@ app.get("/posts/new", (req, res) => {
     res.render("create");
 });
 
-app.post('/posts/store',(req,res)=>{     
-    console.log(req.body) 
+//app.post('/posts/store',(req,res)=>{     
+   // console.log(req.body) 
+    //res.redirect('/') 
+    //})
+
+app.post('/posts/store', async (req,res)=>{             
+    await BlogPost.create(req.body) 
     res.redirect('/') 
-    })
+})
 
 app.listen(3000, () => {
     console.log("App listening on port 3000");
