d8fbbb5e87e7bf07c7764faf5fca9889f17f037e
Nyakallo1996 <Lekgoabamahlaks@gmail.com>
2024-04-24 08:26:53 +0200

    node

--- Tutorial_1_Chapter8/index.js
+++ Tutorial_1_Chapter8/index.js
@@ -8,12 +8,24 @@ const Schema = mongoose.Schema;
 const app = new express();
 const fileUpload = require("express-fileupload");
 
+const customMiddleWare = (req, res, next) => {
+    console.log("Custom middle ware called")
+    next()
+}
 
+const validationMiddleWare = (req, res, next) => {
+    if(req.files == null || req.body.title == null) {
+        return res.redirect("/posts/new")
+    }
+    next()
+}
 
 app.use(express.static('public'));
 app.use(express.json()); 
 app.use(express.urlencoded());
 app.use(fileUpload());
+app.use(customMiddleWare)
+app.use("/posts/store", validationMiddleWare)
 // With app.set('view engine','ejs'), we tell Express to use EJS as our templating engine, 
 // that any file ending in .ejs should be rendered with the EJS package.
 app.set('view engine', 'ejs');
@@ -59,7 +71,7 @@ app.get("/posts/new", (req, res) => {
     //})
 
 app.post("/posts/store", (req,res) => {
-    let image = req.file.image;
+    let image = req.files.image;
     image.mv(path.resolve(__dirname, "public/img", image.name), 
     async(error) => {
     await BlogPost.create({
---
+++ Tutorial_1_Chapter9/index.js
@@ -0,0 +1,68 @@
+const express = require('express');
+
+const app = new express();
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
+const newPostController = require("./controllers/newPost");
+const homeController = require("./controllers/home");
+const storePostController = require("./controllers/storePost");
+const getPostController = require("./controllers/getPost");
+const validateMiddleWare = require("./middleware/validationMiddleware");
+const BlogPost = require('./models/BlogPost.js');
+ 
+const Schema = mongoose.Schema;
+app.use(fileUpload());
+
+mongoose.connect('mongodb://127.0.0.1/my_database', {useNewUrlParser: true});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.listen(3000, () => {
+    console.log("App listening on port 3000");
+});
+
+const customMiddleWare = (req, res, next) => {
+    console.log("Custom middle ware called")
+    next()
+}
+
+
+
+
+
+
+
+
+
+app.use(express.json()); 
+app.use(express.urlencoded());
+
+app.use(customMiddleWare)
+app.use("/posts/store", validateMiddleWare)
+// With app.set('view engine','ejs'), we tell Express to use EJS as our templating engine, 
+// that any file ending in .ejs should be rendered with the EJS package.
+app.set('view engine', 'ejs');
+
+
+
+app.get("/", homeController);
+
+
+app.get("/post/:id", getPostController);
+
+
+
+
+app.get("/posts/new", newPostController);
+
+
+
+app.post("/posts/store", storePostController);
+
+
