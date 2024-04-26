6e5c8a7c327f74fe93a736c2064e3f3e187aa004
Nyakallo1996 <Lekgoabamahlaks@gmail.com>
2024-04-26 17:30:50 +0200

    node

--- Tutorial_1_Chapter7/index.js
+++ Tutorial_1_Chapter7/index.js
@@ -3,23 +3,29 @@ const path = require('path');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 const BlogPost = require('./models/BlogPost.js');
- 
+//const BlogPost = mongoose.model('BlogPost',BlogPostSchema); 
 const Schema = mongoose.Schema;
 const app = new express();
-const fileUpload = require("express-fileupload");
 
-
-
+const BlogPostSchema = new Schema({ 
+    title: String, 
+    body: String, 
+    username: String, 
+    datePosted:{ /* can declare property type with an object like this 
+    because we need 'default' */ 
+    type: Date, 
+    default: new Date() 
+    }   
+    }); 
 
 app.use(express.static('public'));
 app.use(express.json()); 
-app.use(express.urlencoded());
-app.use(fileUpload());
+app.use(express.urlencoded({extended: true})); 
 // With app.set('view engine','ejs'), we tell Express to use EJS as our templating engine, 
 // that any file ending in .ejs should be rendered with the EJS package.
 app.set('view engine', 'ejs');
 mongoose.connect('mongodb://127.0.0.1/my_database', {useNewUrlParser: true});
-
+module.exports = BlogPost 
 //app.get('/', (req, res) => {
 //    res.render('index');
 //});
@@ -29,7 +35,7 @@ app.get('/',async (req,res)=>{
     res.render('index',{ 
     blogposts: blogposts 
     }); 
-    })
+    }) 
 
 app.get('/about', (req, res) => {
     res.render('about');
@@ -43,12 +49,12 @@ app.get('/post', (req, res) => {
     res.render('post');
 });
 
-/*app.get('/post/:id',async (req,res)=>{         
+app.get('/post/:id',async (req,res)=>{         
     const blogpost = await BlogPost.findById(req.params.id) 
     res.render('post',{ 
     blogpost 
-})     
-    })*/
+    })     
+    }) 
 
 app.get("/posts/new", (req, res) => {
     res.render("create");
@@ -59,16 +65,9 @@ app.get("/posts/new", (req, res) => {
     //res.redirect('/') 
     //})
 
-app.post("/posts/store", (req,res) => {
-    let image = req.files.image;
-    image.mv(path.resolve(__dirname, "public/img", image.name), 
-    async(error) => {
-    await BlogPost.create({
-        ...req.body,
-        image: "/img/" + image.name
-    })
-    res.redirect("/")
-})
+app.post('/posts/store', async (req,res)=>{             
+    await BlogPost.create(req.body) 
+    res.redirect('/') 
 })
 
 app.listen(3000, () => {
--- Tutorial_1_Chapter7/models/BlogPost.js
+++ Tutorial_1_Chapter7/models/BlogPost.js
@@ -5,25 +5,18 @@
 // This means that each document in the collection would have the fields specified in the schema.
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-//const BlogPost = mongoose.model('BlogPost',BlogPostSchema);
 
-/*const BlogPostSchema = new Schema({
+const BlogPostSchema = new Schema({
     title: String,
-    body: String
-});*/
-
-const BlogPostSchema = new Schema({ 
-    title: String, 
-    body: String, 
+    body: String,
     username: String, 
-    datePosted:{ /* can declare property type with an object like this 
-    because we need 'default' */ 
-    type: Date, 
-    default: new Date() 
-    }, 
-    image: String 
-    });
-    
+datePosted:{ /* can declare property type with an object like this 
+because we need 'default' */ 
+type: Date, 
+default: new Date() 
+}   
+});
+
 
 // We access the database via mongoose.model. 
 // The first argument is the singular name of the collection your model is for. 
