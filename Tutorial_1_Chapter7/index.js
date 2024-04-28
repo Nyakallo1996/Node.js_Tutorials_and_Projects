f5ea853d506f8cbd30ffde3d8a279598b417438a
Nyakallo1996 <Lekgoabamahlaks@gmail.com>
2024-04-28 09:25:32 +0200

    node

--- Tutorial_1_Chapter7/index.js
+++ Tutorial_1_Chapter7/index.js
@@ -1,75 +1,42 @@
 const express = require('express');
-const path = require('path');
+
+const app = new express();
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const BlogPost = require('./models/BlogPost.js');
-//const BlogPost = mongoose.model('BlogPost',BlogPostSchema); 
-const Schema = mongoose.Schema;
-const app = new express();
+const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
+const newPostController = require('./controllers/newPost');
+const homeController = require('./controllers/home');
+const storePostController = require('./controllers/storePost');
+const getPostController = require('./controllers/getPost');
+const validateMiddleWare = require('./middleware/validationMiddleware');
 
-const BlogPostSchema = new Schema({ 
-    title: String, 
-    body: String, 
-    username: String, 
-    datePosted:{ /* can declare property type with an object like this 
-    because we need 'default' */ 
-    type: Date, 
-    default: new Date() 
-    }   
-    }); 
+app.use(fileUpload());
+
+mongoose.connect('mongodb://localhost/my_database', { useUnifiedTopology: true, useNewUrlParser: true });
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
-app.use(express.json()); 
-app.use(express.urlencoded({extended: true})); 
-// With app.set('view engine','ejs'), we tell Express to use EJS as our templating engine, 
-// that any file ending in .ejs should be rendered with the EJS package.
-app.set('view engine', 'ejs');
-mongoose.connect('mongodb://127.0.0.1/my_database', {useNewUrlParser: true});
-module.exports = BlogPost 
-//app.get('/', (req, res) => {
-//    res.render('index');
-//});
 
-app.get('/',async (req,res)=>{     
-    const blogposts = await BlogPost.find({})      
-    res.render('index',{ 
-    blogposts: blogposts 
-    }); 
-    }) 
-
-app.get('/about', (req, res) => {
-    res.render('about');
+app.listen(4000, () => {
+    console.log('App listening on port 4000 ...');
 });
 
-app.get('/contact', (req, res) => {
-    res.render('contact');
-});
+const myMiddleWare = (req, res, next) => {
+    console.log('Custom middleware called')
+    next()
+};
+app.use(myMiddleWare);
 
-app.get('/post', (req, res) => {
-    res.render('post');
-});
+app.use('/posts/store', validateMiddleWare);
 
-app.get('/post/:id',async (req,res)=>{         
-    const blogpost = await BlogPost.findById(req.params.id) 
-    res.render('post',{ 
-    blogpost 
-    })     
-    }) 
+app.get('/', homeController);
 
-app.get("/posts/new", (req, res) => {
-    res.render("create");
-});
+app.get('/post/:id', getPostController);
 
-//app.post('/posts/store',(req,res)=>{     
-   // console.log(req.body) 
-    //res.redirect('/') 
-    //})
+app.get('/posts/new', newPostController);
 
-app.post('/posts/store', async (req,res)=>{             
-    await BlogPost.create(req.body) 
-    res.redirect('/') 
-})
-
-app.listen(3000, () => {
-    console.log("App listening on port 3000");
-});
+app.post('/posts/store', storePostController);
